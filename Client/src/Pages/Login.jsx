7eff726cc7b2fede62f4b2cc2fcf706395cf038e
Om6124 <omnathganapure9981@gmail.com>
2024-06-27 19:37:18 +0530

    added login backend remaining

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{useState} from "react";
+import '../Pages/Login.css'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [user, setuser] = useState({
+    Email:"",
+    Password:""
+  })
 
-export default Login
+  const handleChange=(e)=>{
+    e.preventDefault();
+    const {name,value}=e.target;
+    setuser({
+      ...user,
+      [name]:value
+    })
+
+  }
+
+  const handleSubmit=()=>{
+
+  }
+
+  
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <div className="form">
+        <label htmlFor="Email">Email : </label>
+        <input type="email" name="Email" id="Email" value={user.Email} onChange={handleChange}/>
+        <label htmlFor="Password">Password : </label>
+        <input type="password" name="Password" id="Password" value={user.Password} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
